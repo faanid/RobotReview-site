a3c9754e9fa99876873749a1e7508173005b3fd2
Fateme Kharazmi <73032767+faanid@users.noreply.github.com>
2023-02-03 22:21:13 +0330

    Countdown 1/2

--- js/app.js
+++ js/app.js
@@ -119,3 +119,53 @@ about.addEventListener("click", function (e) {
     element.classList.add("active");
   }
 });
+
+// ---------
+// countdown
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const giveaway = document.queryselector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+// console.log(items);
+
+let futureDate = new Date(2023, 4, 23, 17, 30, 0);
+//console.log(futureDate);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+const date = future.getDate();
+
+const weekday = weekdays[futureDate.getDate()];
+
+//not complete 5:23:54
+
+giveaway.textContent =
+  "giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am ";
