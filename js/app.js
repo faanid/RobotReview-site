95b4559de3bf84132fd4d3a0727acbecd5dd7f53
Fateme Kharazmi <73032767+faanid@users.noreply.github.com>
2023-02-17 23:57:14 +0330

    add js

--- js/app.js
+++ js/app.js
@@ -445,3 +445,48 @@ function createListItem(id, value) {
 }
 
 //------Slider-----
+const slides = document.querySelectorAll(".slide");
+const nextBtn = document.querySelector(".nextBtn");
+const prevBtn = document.querySelector(".prevBtn");
+
+slides.forEach(function (slide, index) {
+  slide.style.left = `${index * 100}%`;
+});
+
+let counter = 0;
+nextBtn.addEventListener("click", function () {
+  counter++;
+  carousel();
+});
+
+prevBtn.addEventListener("click", function () {
+  counter--;
+  carousel();
+});
+
+function carousel() {
+  // working with slides
+
+  // if (counter === slides.length) {
+  //   counter = 0;
+  // }
+
+  // if (counter < 0) {
+  //   counter = slides.length - 1;
+  // }
+
+  //working with buttons
+  if (counter < slides.length - 1) {
+    nextBtn.style.display = "block";
+  } else {
+    nextBtn.slides.display = "none";
+  }
+  if (counter > 0) {
+    prevBtn.style.display = "block";
+  } else {
+    prevBtn.style.display = "none";
+  }
+  slides.forEach(function (slide) {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
+}
